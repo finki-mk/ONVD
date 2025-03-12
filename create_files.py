fae9b6ca9a38c44078d46bf4bd2f90c880f1de21
Stefan Andonov <stefan5andonov@yahoo.com>
2025-03-12 21:57:35 +0100

    Fix create_files.py script 2

--- create_files.py
+++ create_files.py
@@ -41,5 +41,5 @@ for source_folder in source_folders:
 # 		)
 
 # shutil.copy2("src/img", "output/img")
-shutil.copytree("src/img", "output/output_html/img")
+# shutil.copytree("src/img", "output/output_html/img")
 shutil.copy2("style.css", "output/output_html/mk/style.css")
