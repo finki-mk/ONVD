0fa38cc626a8bf0f622178073a5033e7fd772325
Stefan Andonov <stefan5andonov@yahoo.com>
2025-03-26 00:09:30 +0100

    Fix missing logo and bilingual

--- create_index_pages.py
+++ create_index_pages.py
@@ -8,15 +8,14 @@ def generate_index(html_files_directory):
                   filename.endswith('.html') and not filename.startswith("index")]
     html_files.sort(key=lambda el: int(el.split(".")[0]))  # Sort the filenames numerically
 
-    index_content = '<html>\n<head>\n<meta charset="utf-8" />\n<title>Index</title>\n<link rel="stylesheet" href="style.css" />\n</head>\n<body>\n'
+    index_content = '<html>\n<head>\n<meta charset="utf-8" />\n<title>Auditory exercises in Introduction to Web Design</title>\n<link rel="stylesheet" href="style.css" />\n</head>\n<body>\n'
     index_content += '<img src="../img/logo_mk.png">'
-    index_content += '<h1>Аудиториски вежби по Основи на веб дизајн</h1>\n' if not is_en \
-        else '<h1>Auditory exercises in Structural programming</h1>\n'
+    index_content += '<h1>Аудиториски вежби по Основи на веб дизајн / Auditory exercises in Introduction to Web Design </h1>\n'
 
     index_content += f'<h2>Содржина</h2>\n' if not is_en else "<h2>Content</h2>"
 
     for filename in html_files:
-        index_content += f'<a href="{filename}">{"Аудиториска вежба бр." if not is_en else "Auditory exercise #"} {filename.replace(".html", "")}</a><br>\n'
+        index_content += f'<a href="{filename}">"Аудиториска вежба бр. {filename.replace(".html", "")} / Auditory exercise #{filename.replace(".html", "")}" </a><br>\n'
 
     index_content += '</body>\n</html>'
 
